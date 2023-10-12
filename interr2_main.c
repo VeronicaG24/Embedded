5c8214a8d62dc0c3040a68f58362a7abd76cf576
Gabriele Nicchiarelli <31885249+gabri00@users.noreply.github.com>
2023-10-12 12:30:08 +0200

    little fix

--- interr2_main.c
+++ interr2_main.c
@@ -34,42 +34,99 @@
 #include <xc.h>
 
 #define TIMER1 1
+#define TIMER2 2
 #define FOSC 7372800
 
 void tmr_setup_period(int timer, int ms) {
-    TMR1 = 0; // reset timer counter
+    switch(timer) {
+        case TIMER1: {
+            TMR1 = 0; // reset timer counter
 
-    long fcy = (FOSC / 4) * (ms / 1000.0);
+            long fcy = (FOSC / 4) * (ms / 1000.0);
 
-    long fcy_new = 0.0;
+            long fcy_new = 0.0;
 
-    if (fcy > 65535) {
-        fcy_new = fcy / 8;
-        T1CONbits.TCKPS = 1; // prescaler 1:8
+            if (fcy > 65535) {
+                fcy_new = fcy / 8;
+                T1CONbits.TCKPS = 1; // prescaler 1:8
+            }
+            if (fcy_new > 65535) {
+                fcy_new = fcy / 64;
+                T1CONbits.TCKPS = 2; // prescaler 1:64
+            }
+            if (fcy_new > 65535) {
+                fcy_new = fcy / 256;
+                T1CONbits.TCKPS = 3; // prescaler 1:256
+            }
+
+            PR1 = fcy_new;
+
+            T1CONbits.TON = 1; //starts the timer
+        }
+        break;
+        
+        case TIMER2: {
+            TMR2 = 0; // reset timer counter
+
+            long fcy = (FOSC / 4) * (ms / 1000.0);
+
+            long fcy_new = 0.0;
+
+            if (fcy > 65535) {
+                fcy_new = fcy / 8;
+                T2CONbits.TCKPS = 1; // prescaler 1:8
+            }
+            if (fcy_new > 65535) {
+                fcy_new = fcy / 64;
+                T2CONbits.TCKPS = 2; // prescaler 1:64
+            }
+            if (fcy_new > 65535) {
+                fcy_new = fcy / 256;
+                T2CONbits.TCKPS = 3; // prescaler 1:256
+            }
+
+            PR2 = fcy_new;
+
+            T2CONbits.TON = 1; // starts the timer
+        }
+        break;
     }
-    if (fcy_new > 65535) {
-        fcy_new = fcy / 64;
-        T1CONbits.TCKPS = 2; // prescaler 1:64
-    }
-    if (fcy_new > 65535) {
-        fcy_new = fcy / 256;
-        T1CONbits.TCKPS = 3; // prescaler 1:256
-    }
-
-    PR1 = fcy_new;
-
-    T1CONbits.TON = 1; // starts the timer
 }
 
 void tmr_wait_period(int timer) {
-    while(!IFS0bits.T1IF);
-    IFS0bits.T1IF = 0;
+    switch(timer) {
+        case TIMER1: {
+            while(!IFS0bits.T1IF);
+            IFS0bits.T1IF = 0;
+        }
+        case TIMER2: {
+            while(!IFS0bits.T2IF);
+            IFS0bits.T2IF = 0;
+        }
+    }
 }
 
 void __attribute__ (( __interrupt__ , __auto_psv__ )) _INT0Interrupt() {
     IFS0bits.INT0IF = 0; // reset interrupt flag
 
-    LATBbits.LATB1 = !LATBbits.LATB1;
+    // start timer form 20ms
+    tmr_setup_period(TIMER2, 20);
+}
+
+void __attribute__ (( __interrupt__ , __auto_psv__ )) _T2Interrupt() {
+    IFS0bits.T2IF = 0; // reset interrupt flag
+
+    // when timer elapsed read if the btn is still pressed, if not toggle
+    int pinValue = 0;
+    pinValue = !PORTEbits.RE8;
+    
+    if (!pinValue)
+        T2CONbits.TON = 0; // stop timer
+    else {
+        // stop timer and toggle
+        T2CONbits.TON = 0;
+        LATBbits.LATB1 = !LATBbits.LATB1;
+    }
 }
 
 int main(void) {    
@@ -78,12 +135,11 @@ int main(void) {
     TRISEbits.TRISE8 = 1; // btn S5 as input
 
     IEC0bits.INT0IE = 1; // enable INT0 interrupt
+    IEC0bits.T2IE = 1; // enable INT0 interrupt
 
     tmr_setup_period(TIMER1, 500);
 
     while(1) {
-        IFS0bits.INT0IF = !PORTEbits.RE8;
-
         LATBbits.LATB0 = !LATBbits.LATB0;
         tmr_wait_period(TIMER1);
     }
