debb1c0bde10f36ac6aeff6383126238f98c4438
Gabriele Nicchiarelli <31885249+gabri00@users.noreply.github.com>
2023-11-10 16:24:11 +0100

    circ buff struct and fixes
    Per ora funziona ancora tutto.
    CheckIndex() da riabilitare

--- UART.X/assignment1.c
+++ UART.X/assignment1.c
@@ -37,6 +37,7 @@
 
 #include <xc.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define TIMER1 1
@@ -45,32 +46,36 @@
 #define FOSC 7372800
 #define BAUND 9600
 #define LINE_SIZE 16
-/*
- * baudrate = 9600
- * read buffer every 10ms
- * dimension data transmitted = 1start + 1end + 8data = 10bits
- * (9600bps*0.001s)/10bit = 9.6 --> a little bit bigger: 12
- */
-#define BUFFER_SIZE 12
+#define BUFF_SIZE 12
 #define FIRST_ROW 0x80
 #define SECOND_ROW 0xC0
 #define CR '\r'
 #define LF '\n'
 
-char receivedChar;
-int charCount = 0;
-int rowCount = 0;
-int btn_press = 0; // 0: S5, 1: S6
-
-char circularBuffer[BUFFER_SIZE];
-int readIndex = 0;
-int writeIndex = 0;
+/* How to compute the buffer size:
+ * baudrate = 9600
+ * read time = 10ms
+ * bits transmitted = 1start + 1end + 8data = 10bits
+ * (9600bps*0.001s)/10bits = 9.6 --> a little bit bigger: 12
+ */
 
 typedef struct {
-    char buff[BUFFER_SIZE];
-    int readIdx;
-    int writeIdx;
-} circBuff;
+    char buff[BUFF_SIZE];
+    int size;
+    int readIndex;
+    int writeIndex;
+} CircBuff;
+
+CircBuff buff; // Circular buffer
+
+char receivedChar; // Received character
+int charCount = 0; // Numeber of characters
+int rowCount = 0;  // Row index
+int btn_press = 0; // Flag to select which btn was pressed (0: S5, 1: S6)
+
+char circularBuffer[BUFF_SIZE];
+int readIndex = 0;
+int writeIndex = 0;
 
 void tmr_setup_ms(int timer) {
     switch (timer) {
@@ -94,102 +99,6 @@ void tmr_setup_ms(int timer) {
             T3CONbits.TON = 1; // start T3
         }
         break;
-    }
-}
-
-void tmr_setup_period(int timer, int ms) {
-    switch(timer) {
-        case TIMER1: {
-            TMR1 = 0; // reset T1 counter
-
-            long fcy = (FOSC / 4) * (ms / 1000.0);
-            long fcy_new = fcy;
-
-            if (fcy > 65535) {
-                fcy_new = fcy / 8;
-                T1CONbits.TCKPS = 1; // prescaler 1:8
-            }
-            if (fcy_new > 65535) {
-                fcy_new = fcy / 64;
-                T1CONbits.TCKPS = 2; // prescaler 1:64
-            }
-            if (fcy_new > 65535) {
-                fcy_new = fcy / 256;
-                T1CONbits.TCKPS = 3; // prescaler 1:256
-            }
-
-            PR1 = fcy_new;
-
-            T1CONbits.TON = 1; // start T1
-        }
-        break;
-        
-        case TIMER2: {
-            TMR2 = 0; // reset T2 counter
-
-            long fcy = (FOSC / 4) * (ms / 1000.0);
-            long fcy_new = fcy;
-
-            if (fcy > 65535) {
-                fcy_new = fcy / 8;
-                T2CONbits.TCKPS = 1; // prescaler 1:8
-            }
-            if (fcy_new > 65535) {
-                fcy_new = fcy / 64;
-                T2CONbits.TCKPS = 2; // prescaler 1:64
-            }
-            if (fcy_new > 65535) {
-                fcy_new = fcy / 256;
-                T2CONbits.TCKPS = 3; // prescaler 1:256
-            }
-
-            PR2 = fcy_new;
-
-            T2CONbits.TON = 1; // start T2
-        }
-        break;
-        
-        case TIMER3: {
-            TMR3 = 0; // reset T3 counter
-
-            long fcy = (FOSC / 4) * (ms / 1000.0);
-            long fcy_new = fcy;
-
-            if (fcy > 65535) {
-                fcy_new = fcy / 8;
-                T2CONbits.TCKPS = 1; // prescaler 1:8
-            }
-            if (fcy_new > 65535) {
-                fcy_new = fcy / 64;
-                T2CONbits.TCKPS = 2; // prescaler 1:64
-            }
-            if (fcy_new > 65535) {
-                fcy_new = fcy / 256;
-                T2CONbits.TCKPS = 3; // prescaler 1:256
-            }
-
-            PR3 = fcy_new;
-
-            T3CONbits.TON = 1; // start T3
-        }
-        break;
-    }
-}
-
-void tmr_wait_period(int timer) {
-    switch(timer) {
-        case TIMER1: {
-            while(!IFS0bits.T1IF);
-            IFS0bits.T1IF = 0;
-        } break;
-        case TIMER2: {
-            while(!IFS0bits.T2IF);
-            IFS0bits.T2IF = 0;
-        } break;
-        case TIMER3: {
-            while(!IFS0bits.T3IF);
-            IFS0bits.T3IF = 0;
-        } break;
     }
 }
 
@@ -244,9 +153,70 @@ void tmr_wait_ms(int timer, int ms) {
     }
 }
 
+void tmr_setup_period(int timer, int ms) {
+    int prescaler = 1;
+    long fcy = (FOSC / 4) * (ms / 1000.0);
+    long fcy_new = fcy;
+
+    if (fcy > 65535) {
+        fcy_new = fcy / 8;
+        prescaler = 1; // prescaler 1:8
+    }
+    if (fcy_new > 65535) {
+        fcy_new = fcy / 64;
+        prescaler = 2; // prescaler 1:64
+    }
+    if (fcy_new > 65535) {
+        fcy_new = fcy / 256;
+        prescaler = 3; // prescaler 1:256
+    }
+
+    switch(timer) {
+        case TIMER1: {
+            TMR1 = 0; // reset T1 counter
+            T1CONbits.TCKPS = prescaler;
+            PR1 = fcy_new;
+            T1CONbits.TON = 1; // start T1
+        }
+        break;
+        
+        case TIMER2: {
+            TMR2 = 0; // reset T2 counter
+            T2CONbits.TCKPS = prescaler;
+            PR2 = fcy_new;
+            T2CONbits.TON = 1; // start T2
+        }
+        break;
+        
+        case TIMER3: {
+            TMR3 = 0; // reset T3 counter
+            T1CONbits.TCKPS = prescaler;
+            PR3 = fcy_new;
+            T3CONbits.TON = 1; // start T3
+        }
+        break;
+    }
+}
+
+void tmr_wait_period(int timer) {
+    switch(timer) {
+        case TIMER1: {
+            while(!IFS0bits.T1IF);
+            IFS0bits.T1IF = 0;
+        } break;
+        case TIMER2: {
+            while(!IFS0bits.T2IF);
+            IFS0bits.T2IF = 0;
+        } break;
+        case TIMER3: {
+            while(!IFS0bits.T3IF);
+            IFS0bits.T3IF = 0;
+        } break;
+    }
+}
+
 void __attribute__ (( __interrupt__ , __auto_psv__ )) _INT0Interrupt() {
     IFS0bits.INT0IF = 0; // reset interrupt flag
-
     btn_press = 0;
     // start timer form 20ms
     tmr_setup_period(TIMER2, 20);
@@ -254,7 +224,6 @@ void __attribute__ (( __interrupt__ , __auto_psv__ )) _INT0Interrupt() {
 
 void __attribute__ (( __interrupt__ , __auto_psv__ )) _INT1Interrupt() {
     IFS1bits.INT1IF = 0; // reset interrupt flag
-
     btn_press = 1;
     // start timer form 20ms
     tmr_setup_period(TIMER2, 20);
@@ -274,7 +243,6 @@ void __attribute__ (( __interrupt__ , __auto_psv__ )) _T2Interrupt() {
     T2CONbits.TON = 0; // stop T2
     
     if (pinValue) {
-
         if (!btn_press) {
             // Send the current number of characters received via UART2
             char buff[LINE_SIZE];
@@ -291,8 +259,64 @@ void __attribute__ (( __interrupt__ , __auto_psv__ )) _T2Interrupt() {
             charCount = 0;
             UpdateSecondRow();
         }
-
     }
+}
+
+void __attribute__((__interrupt__, __auto_psv__)) _U2RXInterrupt() {
+    IFS1bits.U2RXIF = 0;  // Reset del flag di interrupt
+    // Check for received characters from UART2
+    
+    while (U2STAbits.URXDA) {
+        //checkIndexes();
+        receivedChar = U2RXREG;
+        circularBuffer[readIndex % BUFF_SIZE] = receivedChar;
+        readIndex++;
+    }
+}
+
+// Function to initialize the circular buffer
+void Buff_Init(CircBuff* buff)
+{
+    buff->size = 0;
+    buff->readIndex = 0;
+    buff->writeIndex = 0;
+}
+
+// Function to check if the circular buffer is empty
+int Buff_IsEmpty(const CircBuff *buff)
+{
+    return buff->size == 0;
+}
+
+// Function to check if the circular buffer is full
+int Buff_IsFull(const CircBuff *buff)
+{
+    return buff->size == BUFF_SIZE;
+}
+
+// Function to write a char to the circular buffer
+void Buff_Write(CircBuff *buff, char data)
+{
+    if (!Buff_IsFull(buff))
+    {
+        buff->buff[buff->writeIndex] = data;
+        buff->writeIndex = (buff->writeIndex + 1) % BUFF_SIZE;
+        buff->size++;
+    }
+    // Note: You might want to handle the case when the buffer is full (e.g., drop data or raise an error)
+}
+
+// Function to read a char from the circular buffer
+char Buff_Read(CircBuff *buff)
+{
+    char data = '\0'; // Default value if the buffer is empty
+    if (!Buff_IsEmpty(buff))
+    {
+        data = buff->buff[buff->readIndex];
+        buff->readIndex = (buff->readIndex + 1) % BUFF_SIZE;
+        buff->size--;
+    }
+    return data;
 }
 
 // Function to initialize SPI1 for LCD
@@ -363,37 +387,26 @@ void algorithm() {
     tmr_wait_ms(TIMER3, 7);
 }
 
-void __attribute__((interrupt, auto_psv)) _U2RXInterrupt(void) {
-    IFS1bits.U2RXIF = 0;  // Reset del flag di interrupt
-    // Check for received characters from UART2
-    
-    while (U2STAbits.URXDA) {
-        //checkIndexes();
-        receivedChar = U2RXREG;
-        circularBuffer[readIndex % BUFFER_SIZE] = receivedChar;
-        readIndex++;
-    }
-}
-
-int checkByteAveilable() {
+int checkAvailableBytes() {
     if(writeIndex <= readIndex) {
         return readIndex - writeIndex;
     }
     else {
-        return BUFFER_SIZE - writeIndex + readIndex;
+        return BUFF_SIZE - writeIndex + readIndex;
     }
 }
 
 void checkIndexes() {
-    if((writeIndex % BUFFER_SIZE) - (readIndex % BUFFER_SIZE) < 2 && readIndex > BUFFER_SIZE) {
+    if((writeIndex % BUFF_SIZE) - (readIndex % BUFF_SIZE) < 2 && readIndex > BUFF_SIZE) {
         writeIndex++;
     }
 }
 
 
 int main(void) {
+    // Init timer
     tmr_setup_ms(TIMER3);
-    
+
     // Init UART2 and SPI1
     UART2_Init();
     SPI1_Init();
@@ -409,21 +422,22 @@ int main(void) {
     U2STAbits.URXISEL = 3; //mode UART2 interrupt 1: every char received, 2: 3/4 buffer, 3: full
 
     tmr_setup_period(TIMER1, 10);
+
     while (1) {
         algorithm();
         
-        IEC1bits.U2RXIE = 0; // disable UART2 interrupt
+        IEC1bits.U2RXIE = 0; // Disable UART2 interrupt
         while (U2STAbits.URXDA) {
             //checkIndexes();
             receivedChar = U2RXREG;
-            circularBuffer[readIndex % BUFFER_SIZE] = receivedChar;
+            circularBuffer[readIndex % BUFF_SIZE] = receivedChar;
             readIndex++;
         }
-        IEC1bits.U2RXIE = 1; // enable UART2 interrupt
+        IEC1bits.U2RXIE = 1; // Enable UART2 interrupt
         
         // Check for CR and LF characters and handle accordingly
-        if (checkByteAveilable() > 0) {
-            if (circularBuffer[writeIndex % BUFFER_SIZE] == CR || circularBuffer[writeIndex % BUFFER_SIZE] == LF) {
+        if (checkAvailableBytes() > 0) {
+            if (circularBuffer[writeIndex % BUFF_SIZE] == CR || circularBuffer[writeIndex % BUFF_SIZE] == LF) {
                 LCD_ClearFirstRow();
                 writeIndex++;
             }
@@ -433,7 +447,7 @@ int main(void) {
             else {
                 LCD_SendData(FIRST_ROW + rowCount);
                 // Display the received character on the first row of the LCD
-                LCD_SendData(circularBuffer[writeIndex % BUFFER_SIZE]);
+                LCD_SendData(circularBuffer[writeIndex % BUFF_SIZE]);
                 rowCount++;
                 charCount++;
                 // Update the second row with the character count
