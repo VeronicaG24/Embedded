19dd0d9f7dd3ebfa9b5a57f2b04d32df81a69aca
Gabriele Nicchiarelli <31885249+gabri00@users.noreply.github.com>
2023-11-16 13:47:41 +0100

    timer ex new board

--- TestNewBoard.X/main.c
+++ TestNewBoard.X/main.c
@@ -43,7 +43,126 @@
 
 #include <xc.h>
 
-int main() {
+#define TIMER1 1
+#define TIMER2 2
+#define FOSC 8000000
+
+void tmr_setup_period(int timer, int ms) {
+    // Fcy = Fosc / 2 = 8000000 / 2 = 4000000 (number of clocks in one second)
+    // in 0.1 second there would be 400000 clocks steps
+    // If we set a prescaler of 1:8 we have 400000/8 = 50000 clock steps, OK!
+    int prescaler = 1;
+    long fcy = (FOSC / 2) * (ms / 1000.0);
+    long fcy_new = fcy;
+
+    if (fcy > 65535) {
+        fcy_new = fcy / 8;
+        prescaler = 1; // prescaler 1:8
+    }
+    if (fcy_new > 65535) {
+        fcy_new = fcy / 64;
+        prescaler = 2; // prescaler 1:64
+    }
+    if (fcy_new > 65535) {
+        fcy_new = fcy / 256;
+        prescaler = 3; // prescaler 1:256
+    }
+
+    switch(timer) {
+        case TIMER1: {
+            TMR1 = 0; // reset T1 counter
+            T1CONbits.TCKPS = prescaler;
+            PR1 = fcy_new;
+            T1CONbits.TON = 1; // start T1
+        }
+        break;
+        
+        case TIMER2: {
+            TMR2 = 0; // reset T2 counter
+            T2CONbits.TCKPS = prescaler;
+            PR2 = fcy_new;
+            T2CONbits.TON = 1; // start T2
+        }
+        break;
+    }
+}
+
+void tmr_wait_period(int timer) {
+    switch(timer) {
+        case TIMER1: {
+            while(!IFS0bits.T1IF);
+            IFS0bits.T1IF = 0;
+        }
+        break;
+
+        case TIMER2: {
+            while(!IFS0bits.T2IF);
+            IFS0bits.T2IF = 0;
+        }
+        break;
+    }
+}
+
+void tmr_setup_ms(int timer) {
+    switch (timer) {
+        case TIMER1: {
+            TMR1 = 0; // reset T1 counter
+            IFS0bits.T1IF = 0; // reset T1 flag
+            T1CONbits.TON = 1; // start T1
+        }
+        break;
+        
+        case TIMER2: {
+            TMR2 = 0; // reset T2 counter
+            IFS0bits.T2IF = 0; // reset T2 flag
+            T2CONbits.TON = 1; // start T2
+        }
+        break;
+    }
+}
+
+void tmr_wait_ms(int timer, int ms) {
+    int prescaler = 1;
+    long fcy = (FOSC / 2) * (ms / 1000.0);
+    long fcy_new = fcy;
+
+    if (fcy > 65535) {
+        fcy_new = fcy / 8;
+        prescaler = 1; // prescaler 1:8
+    }
+    if (fcy_new > 65535) {
+        fcy_new = fcy / 64;
+        prescaler = 2; // prescaler 1:64
+    }
+    if (fcy_new > 65535) {
+        fcy_new = fcy / 256;
+        prescaler = 3; // prescaler 1:256
+    }
+
+    switch (timer) {
+        case TIMER1: {
+            T1CONbits.TCKPS = prescaler;
+            PR1 = fcy_new;
+
+            // wait...
+            while(!IFS0bits.T1IF);
+            IFS0bits.T1IF = 0;
+        }
+        break;
+
+        case TIMER2: {
+            T2CONbits.TCKPS = prescaler;
+            PR2 = fcy_new;
+
+            // wait...
+            while(!IFS0bits.T2IF);
+            IFS0bits.T2IF = 0;
+        }
+        break;
+    }
+}
+
+void Init_Timer() {
     PLLFBD = 6; // M = 8
     CLKDIVbits.PLLPOST = 0x00; // N1 = 2
     CLKDIVbits.PLLPRE = 0x00; // N2 = 2
@@ -51,15 +170,43 @@ int main() {
 
     while (OSCCONbits.LOCK != 1) {
     }; // Wait for PLL to lock
+}
+
+int main() {
+    Init_Timer();
+
+    TRISAbits.TRISA0 = 0; // led A0 as output
+    TRISEbits.TRISE8 = 1; // btn E8 as input
+
+    int btnValue = 0; // btn E8 value
+    int period = 1000; // ms
+    int pulse = 100; // ms
+    int mult = 0; // multiplier: 1, 2, 3
+
+    // setup and start timer
+    tmr_setup_ms(TIMER1);
     
-    TMR1 = 0; // reset timer counter
-// Fcy = Fosc / 2 = 8000000 / 2 = 4000000 (number of clocks in one second)
-// in 0.1 second there would be 400000 clocks steps
-// this is too high to be put in a 16 bit register (max 65535)
-// If we set a prescaler of 1:8 we have 400000/8 = 50000 clock steps, OK!
-    PR1 = 50000; // (8 MHz / 2) / 8 / 10 < 65535!
-    T1CONbits.TCKPS = 1; // prescaler 1:8
-    T1CONbits.TON = 1; // starts the timer!
+    LATAbits.LATA0 = 0; // initially led off
+
+    while (1) {
+        btnValue = PORTEbits.RE8;
+
+        if (!btnValue) mult++;
+
+        if (mult > 0) {
+            if (mult > 3) mult = 1;
+
+            for (int i=0; i<mult; i++) {
+                LATAbits.LATA0 = 1;
+                tmr_wait_ms(TIMER1, pulse);
+
+                LATAbits.LATA0 = 0;
+                tmr_wait_ms(TIMER1, pulse);
+            }
+
+            tmr_wait_ms(TIMER1, period - 2*mult*pulse);
+        }
+    }
 
     return 0;
 }
